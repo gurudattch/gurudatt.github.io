d5b2a0d3256fae85ac5942cf7677a36f7d5b70a3
Gurudatt Choudhary <119776344+gurudattch@users.noreply.github.com>
2024-12-24 11:24:03 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+// This function will completely clear the page and replace it with "Hacked"
+function hackPage() {
+    // Clear the entire document
+    document.documentElement.innerHTML = '<h1 style="text-align: center; color: red;">Hacked</h1>';
+}
+
+// Call the function to execute it
+hackPage();
